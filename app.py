f31349eecd0ac76c676945e8d689622c42d4ec2f
Md Ammar <ammaratif559@gmail.com>
2024-08-06 12:48:29 +0530

    some updations

--- app.py
+++ app.py
@@ -9,24 +9,26 @@ st.text("The dataset looks like this:")
 st.write(df.head())
 
 st.markdown("Use inbuilt dataset or upload your own!")
-st.file_uploader("Upload a file", type=["csv"])
-
+data = st.file_uploader("Upload a file", type=["csv"])
 
 #setting test size
 c1, c2 = st.columns(2)
 c1.text("Set the test size for the model")
 test_size = c2.slider("Test size", 0.1, 1.0, 0.2)
 
-score = 0
+score = None
 
 if st.button("Run model"):
     run_model(test_size)
     st.success("Model trained successfully!")
-    new_score = get_score()
-    st.metric("Model score: ", value=new_score, delta=new_score-score, delta_color="inverse")
-    score=new_score
+    score = get_score()
+    st.metric("Model score: ", value=score)
 
 #predicting
+def predict(sepal_length, sepal_width, petal_length, petal_width):
+    global prediction
+    prediction = Model.predict([[sepal_length, sepal_width, petal_length, petal_width]])[0]
+    
 st.text("Lets make a prediction!")
 
 c1, c2, c3, c4 = st.columns(4)
@@ -38,12 +40,7 @@ petal_width = c4.slider('Petal Width', 0.1, 2.5, 0.2)
 
 st.table({"Sepal Length": sepal_length, "Sepal Width": sepal_width, "Petal Length": petal_length, "Petal Width": petal_width})
 
-def predict(sepal_length, sepal_width, petal_length, petal_width):
-    global prediction
-    prediction = Model.predict([[sepal_length, sepal_width, petal_length, petal_width]])[0]
-
 if st.button("Predict", on_click=predict(sepal_length, sepal_width, petal_length, petal_width)):
     predict(sepal_length, sepal_width, petal_length, petal_width)
     st.text("The model predicts: " + str(prediction))
     st.text("The predicted species is: "+data.target_names[prediction])
-    
